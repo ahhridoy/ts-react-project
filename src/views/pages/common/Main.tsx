cd12a27a2173ea47e4b6e6de02f81e5f1fa88cc4
ahhridoy <ahhridoyy28@gmail.com>
2022-03-11 12:20:57 +0600

    code pattern added

--- src/views/pages/common/Main.tsx
+++ src/views/pages/common/Main.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+// import { Home } from "../home/Home";
+
+// const OtherComponent = React.lazy(() => import('./OtherComponent'));
+const Home = React.lazy(() => import("../home/Home"));
+// code-splitting
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="container mx-auto">
+      <Suspense fallback={<>Loading...</>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={{}} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default Main;
